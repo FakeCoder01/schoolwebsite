ca2d851df466f407e94e27a493e36b5e9aec05cf
FakeCoder01 <97719684+FakeCoder01@users.noreply.github.com>
2022-08-01 17:05:04 +0530

    Update forms.py

--- app/forms.py
+++ app/forms.py
@@ -4,18 +4,20 @@ from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm, UserModel
 from .models import student, teacher
 
+
+# for the student creation of student model
 class studentProfileForm(ModelForm):
     class Meta:
         model = student
         fields = ('full_name', 'gender', 'standard', 'stream', 'section', 'roll_no', 'contact', 'address', 'img')
 
-
+# for the teacher creation of teacher model
 class teacherRegForm(ModelForm):
     class Meta:
         model = teacher
         fields = ('full_name', 'gender', 'stream', 'classes_taught', 'contact', 'address', 'img')
 
-
+# user registraion from for User
 class userRegForm(UserCreationForm):
     class Meta:
         model = User
@@ -26,6 +28,7 @@ class userEmailForm(UserCreationForm):
         model = User
         fields = ['username', 'email',]
 
+# user login from for User
 class userLoginForm(ModelForm):
     class Meta:
         model = User
